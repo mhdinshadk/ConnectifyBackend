c94a15773d167e162e9895227180cdf3fd5cfd68
mhdinshadk <innuinshad2004@gmail.com>
2025-10-25 15:48:49 +0530

    bug fixes

--- index.js
+++ index.js
@@ -28,7 +28,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://connectify-frontent.vercel.app',
+  credentials: true, // if using cookies/auth
+}));
 
 // Ensure the 'public/assets' folder exists
 const assetsDir = path.join(__dirname, 'public/assets');
